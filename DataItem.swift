f24a65c20995f1371bced76b050ce67a34d3be90
Isaac Kim <myong328@gmail.com>
2017-08-29 11:04:39 -0700

    initial commit

---
+++ DataItem.swift
@@ -0,0 +1,25 @@
+//
+//  DataItem.swift
+//  TableViewDemo
+//
+//  Created by Isaac Kim on 8/26/17.
+//  Copyright © 2017 Isaac Kim. All rights reserved.
+//
+
+import UIKit
+
+class DataItem {
+    var title: String
+    var subtitle: String
+    var image: UIImage?
+    
+    init(title: String, subtitle: String, imageName: String?) {
+        self.title = title
+        self.subtitle = subtitle
+        if let imageName = imageName {
+            if let img = UIImage(named: imageName) {
+                image = img
+            }
+        }
+    }
+}
